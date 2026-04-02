5b70c0e5cf66476317fc5a9b339db01a62259b85
Vinay Kumar <vkr7.info@gmail.com>
2026-04-03 00:44:42 +0530

    solved-api-bugs

--- backend/utils/apiResponse.js
+++ backend/utils/apiResponse.js
@@ -1,7 +1,21 @@
-const apiResponse = (statusCode, message, data = null) => {
+const buildResponse = (statusCode, message, data = null) => {
     const response = { success: statusCode < 400, message };
     if (data !== null) response.data = data;
     return response;
+};
+
+const isExpressResponse = (value) =>
+    value &&
+    typeof value.status === "function" &&
+    typeof value.json === "function";
+
+const apiResponse = (resOrStatusCode, statusCodeOrMessage, messageOrData = null, data = null) => {
+    if (isExpressResponse(resOrStatusCode)) {
+        const response = buildResponse(statusCodeOrMessage, messageOrData, data);
+        return resOrStatusCode.status(statusCodeOrMessage).json(response);
+    }
+
+    return buildResponse(resOrStatusCode, statusCodeOrMessage, messageOrData);
 };
 
 export default apiResponse;
